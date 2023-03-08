7dc4203268db78a3076b7d8915f638eadd20fb76
Caio Moreira de Carvalho <103529122+caiomoreiradc@users.noreply.github.com>
2023-03-08 17:25:54 -0300

    Post Inicial dos Arquivos
    08/03/2023 17:25

---
+++ CalculadoraAcademia/Calculadora.ConsoleApp/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Calculadora.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculadora da academia!");
+            Console.Write("Insira o primeiro número: ");
+            decimal num1 = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Insira o segundo número: ");
+            decimal num2 = decimal.Parse(Console.ReadLine());
+
+            string operacao = "";
+            decimal resultado = 0;
+
+            while (true)
+            {
+                Console.WriteLine("1- Realizar Soma:");
+                Console.WriteLine("2- Realizar subtração:");
+                Console.WriteLine("3- Realizar multiplicação:");
+                Console.WriteLine("4- Realizar Divisão:");
+                Console.WriteLine("5- Fechar Programa:");
+                operacao = Console.ReadLine();
+
+                switch (operacao)
+                {
+                    case "1":
+                        resultado = num1 + num2;
+                        break;
+                    case "2":
+                        resultado = num1 - num2;
+                        break;
+                    case "3":
+                        resultado = num1 * num2;
+                        break;
+                    case "4":
+                        resultado = num1 / num2;
+                        break;                    
+                    case "5":
+                        break;
+                    default:
+                        Console.WriteLine("Operação inválida\n");
+                        continue;
+                }
+                break;
+            }
+
+            Console.WriteLine("O resultado da operação é: " + Math.Round(resultado, 2));
+            Console.ReadLine();
+
+        }
+    }
+}
